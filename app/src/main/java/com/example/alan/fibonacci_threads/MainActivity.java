62e31e614339db726fd1d54fe35ad61c58b05adb
Alan Sanchez <sanchez2597asp@outlook.com>
2015-05-08 18:01:28 -0500

    Created thread

--- app/src/main/java/com/example/alan/fibonacci_threads/MainActivity.java
+++ app/src/main/java/com/example/alan/fibonacci_threads/MainActivity.java
@@ -1,17 +1,124 @@
 package com.example.alan.fibonacci_threads;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private EditText inputNumber;
+    private TextView outputView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputNumber = (EditText) findViewById(R.id.numberTextField);
+        outputView = (TextView) findViewById(R.id.resultTextView);
+    }
+
+    public void calcularOperacion(View view) {
+        int n = Integer.parseInt(inputNumber.getText().toString());
+        if(n < 1) {
+            outputView.append("No es posible calcular posiciones negativas o la posicion 0 de la serie de fibonnaci.\n");
+        }
+        else {
+            outputView.append("Fibonacci[" +n + "] = ");
+            FibonacciOperationThread fibonacciThread = new FibonacciOperationThread();
+            fibonacciThread.execute(n);
+        }
+    }
+
+    class FibonacciOperationThread extends AsyncTask<Integer, Integer, Integer> {
+
+        private ProgressDialog progressDialog;
+
+        @Override protected void onPreExecute() {
+
+            progressDialog = new ProgressDialog(MainActivity.this);
+
+            progressDialog.setProgressStyle(ProgressDialog.
+                    STYLE_HORIZONTAL);
+
+            progressDialog.setMessage("Calculando...");
+
+            progressDialog.setCancelable(true);
+
+            progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+                @Override
+                public void onCancel(DialogInterface dialog) {
+
+                    FibonacciOperationThread.this.cancel(true);
+
+                }
+
+            });
+
+            progressDialog.setMax(100);
+
+            progressDialog.setProgress(0);
+
+            progressDialog.show();
+
+        }
+
+        @Override protected Integer doInBackground(Integer... n) {
+
+            int result = 1;
+            int n1 = 1, n2 = 1;
+            int fibonacciPosition = n[0];
+
+            if(fibonacciPosition == 1) {
+                return 0;
+            }
+            else if(fibonacciPosition == 2 || fibonacciPosition == 3) {
+                return 1;
+            }
+            else {
+                for (int i = 4; i <= fibonacciPosition && !isCancelled(); i++) {
+                    n2 = n1;
+                    n1 = result;
+                    result = n1 + n2;
+
+                    SystemClock.sleep(150);
+
+                    publishProgress(i*100 / n[0]);
+
+                }
+                return result;
+            }
+        }
+
+        @Override protected void onProgressUpdate(Integer... porc) {
+
+            progressDialog.setProgress(porc[0]);
+
+        }
+
+        @Override protected void onPostExecute(Integer res) {
+
+            progressDialog.dismiss();
+
+            outputView.append(res + "\n");
+
+        }
+
+        @Override protected void onCancelled() {
+
+            outputView.append("Cancelado.\n");
+
+        }
+
     }
 
 
